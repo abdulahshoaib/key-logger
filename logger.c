38a4c9ca494ebcd00f3193b6c4b7952b38eab8ae
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-09 19:19:14 +0500

    feat: Window added plus rendering of text

--- logger.c
+++ logger.c
@@ -1,3 +1,8 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_video.h>
 #include <fcntl.h>
 #include <linux/input-event-codes.h>
 #include <linux/input.h>
@@ -5,7 +10,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-const char *keycode_map[256] = {
+const char *keycodeMap[256] = {
     [KEY_A] = "a",           [KEY_B] = "b",           [KEY_C] = "c",
     [KEY_D] = "d",           [KEY_E] = "e",           [KEY_F] = "f",
     [KEY_G] = "g",           [KEY_H] = "h",           [KEY_I] = "i",
@@ -42,16 +47,82 @@ int main(void) {
     return EXIT_FAILURE;
   }
 
+  SDL_Init(SDL_INIT_VIDEO);
+  TTF_Init();
+
+  SDL_DisplayMode mode;
+  SDL_GetCurrentDisplayMode(0, &mode);
+
+  int win_w = 300;
+  int win_h = 200;
+
+  int x = mode.w - win_w;
+  int y = mode.h - win_h;
+
+  SDL_Window *win =
+      SDL_CreateWindow("keylogger", x, y, 200, 50, SDL_WINDOW_SHOWN);
+
+  int r = 48;
+  int g = 48;
+  int b = 48;
+  int a = 255;
+
+  SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+  SDL_SetRenderDrawColor(ren, r, g, b, a);
+  SDL_RenderClear(ren);
+  SDL_RenderPresent(ren);
+
+  TTF_Font *font = TTF_OpenFont(
+      "/home/vexatious/nerd-fonts/VictorMonoNFNerdFont-Medium.ttf", 28);
+
+  if (!font) {
+    fprintf(stderr, "TTF font load failed\n");
+    return EXIT_FAILURE;
+  }
+
   struct input_event event;
+  SDL_Event sdl_event;
+  char currentKey[64] = "";
 
   while (1) {
-    read(fd, &event, sizeof(struct input_event));
-    if (event.type == EV_KEY && event.value == 1) {
-      const char *key = keycode_map[event.code];
-      printf("%s ", key);
-      fflush(stdout);
+    SDL_SetWindowPosition(win, x, y);
+
+    while (SDL_PollEvent(&sdl_event)) {
+      if (sdl_event.type == SDL_QUIT)
+        goto cleanup;
+    }
+    if (read(fd, &event, sizeof(event)) == sizeof(event)) {
+      if (event.type == EV_KEY && event.value == 1) {
+        const char *k = keycodeMap[event.code];
+        if (k) {
+          snprintf(currentKey, sizeof(currentKey), "%s", k);
+
+          // Render key
+          SDL_Color white = {255, 255, 255};
+          SDL_Surface *surf = TTF_RenderUTF8_Blended(font, currentKey, white);
+          SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, surf);
+          SDL_Rect dst = {10, 5, surf->w, surf->h};
+
+          SDL_SetRenderDrawColor(ren, r, g, b, a);
+          SDL_RenderClear(ren);
+          SDL_RenderCopy(ren, tex, NULL, &dst);
+          SDL_RenderPresent(ren);
+
+          SDL_FreeSurface(surf);
+          SDL_DestroyTexture(tex);
+        }
+      }
     }
   }
+
+cleanup:
+  TTF_CloseFont(font);
+  SDL_DestroyRenderer(ren);
+  SDL_DestroyWindow(win);
+  TTF_Quit();
+  SDL_Quit();
+  close(fd);
+  return 0;
 
   return EXIT_SUCCESS;
 }
